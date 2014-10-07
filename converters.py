ba85a371b43cf02b14ce9e353f9d8bbc804f1b1a
Rod Wilhelmy <rwilhelmy@gmail.com>
2014-10-07 10:57:02 -0500

    Add owner org to fetched datasets

--- converters.py
+++ converters.py
@@ -24,8 +24,10 @@ def dcat_to_ckan(dcat_dict):
 
     dcat_publisher = dcat_dict.get('publisher')
     if isinstance(dcat_publisher, basestring):
+        package_dict['owner_org'] = dcat_publisher.lower()
         package_dict['extras'].append({'key': 'dcat_publisher_name', 'value': dcat_publisher})
     elif isinstance(dcat_publisher, dict) and dcat_publisher.get('name'):
+        package_dict['owner_org'] = dcat_publisher.get('name').lower()
         package_dict['extras'].append({'key': 'dcat_publisher_name', 'value': dcat_publisher.get('name')})
         package_dict['extras'].append({'key': 'dcat_publisher_email', 'value': dcat_publisher.get('mbox')})
 
