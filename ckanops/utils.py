48b8cf7bb6638cf8bb9dbe142168c965577a3acd
KielRodriguez <kr.rdz.20@gmail.com>
2017-11-11 09:41:04 -0600

    use property context

--- ckanops/utils.py
+++ ckanops/utils.py
@@ -10,7 +10,7 @@ def dcat_to_utf8_dict(url):
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
-    return json.loads(urllib2.urlopen(url, contex=ctx).read().decode('utf-8'))
+    return json.loads(urllib2.urlopen(url, context=ctx).read().decode('utf-8'))
 
 
 def get_extension_from_url(u):
